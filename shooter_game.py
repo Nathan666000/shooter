f230785c5d404a8a53d1ea6f8ba12ab3c7992e2b
Nathan666000 <116495349+Nathan666000@users.noreply.github.com>
2022-10-23 16:06:23 +0300

    Play

---
+++ shooter_game.py
@@ -0,0 +1,30 @@
+#Создай собственный Шутер!
+from pygame import *
+#классы 
+#
+
+#констант 
+win_hieght = 500 
+win_width = 700 
+clock = timeClock()
+Fps = 60 
+
+
+#картинки
+background_img = 'vscode/Картинки/galaxy.jpg'
+background_img = 
+
+#Звуки
+#
+
+#Шрифты 
+#
+
+#окно
+#
+
+#спрайты
+#
+
+#игровой цикл
+#
